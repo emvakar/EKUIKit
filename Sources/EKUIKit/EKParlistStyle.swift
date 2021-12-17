3fa3294950cc73c99cce50658601bb2e83231445
Denis Vasilev <lyon_den@mail.ru>
2021-12-18 01:29:36 +0300

    add public color protocol

---
+++ Sources/EKUIKit/EKParlistStyle.swift
@@ -0,0 +1,54 @@
+//
+//  EKParlistStyle.swift
+//  
+//
+//  Created by Денис Васильев on 18.12.2021.
+//
+
+import UIKit
+
+public protocol EKParlistStyle {
+    
+    var color1: UIColor { get }
+    var color2: UIColor { get }
+    var color3: UIColor { get }
+    var color4: UIColor { get }
+    var color5: UIColor { get }
+    var color6: UIColor { get }
+    var color7: UIColor { get }
+    var color8: UIColor { get }
+    var color9: UIColor { get }
+    var color10: UIColor { get }
+    var color11: UIColor { get }
+    var color12: UIColor { get }
+    var color13: UIColor { get }
+    var color14: UIColor { get }
+    var color15: UIColor { get }
+    var color16: UIColor { get }
+    var color17: UIColor { get }
+    var color18: UIColor { get }
+    var color19: UIColor { get }
+    var color20: UIColor { get }
+    var color21: UIColor { get }
+    var color22: UIColor { get }
+    var color23: UIColor { get }
+    var color24: UIColor { get }
+    var color25: UIColor { get }
+    var color26: UIColor { get }
+    var color27: UIColor { get }
+    var color28: UIColor { get }
+    var color29: UIColor { get }
+    
+    var largeTitle: UIFont { get }
+    var title1: UIFont { get }
+    var bodyText15: UIFont { get }
+    var bodyText: UIFont { get }
+    var bodyTextBold: UIFont { get }
+    var strikethroughText: UIFont { get }
+    var caption1: UIFont { get }
+    var caption1Bold: UIFont { get }
+    var title2: UIFont { get }
+    var h3: UIFont { get }
+    var maxTitle: UIFont { get }
+    
+}
